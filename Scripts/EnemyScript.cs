89eb6c3e225f3c6332f80df91f12e174fe1b68f2
force-flow <33775978+force-flow@users.noreply.github.com>
2019-06-06 15:57:07 +0900

    Add files via upload

---
+++ Scripts/EnemyScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    NavMeshAgent enemy;
+    static int score = 0;
+    public Text text;
+
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+         text.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(player.transform.position);
+    }
+
+    void OnCollisionEnter(Collision col)
+        {
+        	if(col.gameObject.tag=="Bullet"){
+        		Destroy(gameObject);
+        		score++;
+        		if(score >= 5){
+	                SceneManager.LoadScene(0);
+	                score = 0;
+        		}
+        	}
+        }
+}
---
+++ Scripts/PlayerSript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerSript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    public GameObject bullet;
+    GameObject BulletClone;
+    Rigidbody clone;
+    public Text text;
+    int hp = 15;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        text.text = "Health: " + hp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     	float MoveVert = Input.GetAxis("Vertical");
+    	rb.AddForce(transform.forward * MoveVert * 20f); 
+
+        float Rotate = Input.GetAxis("Horizontal");
+        transform.Rotate(0, Rotate * 5f, 0); 
+
+        if(Input.GetKeyDown("space")) {
+        	BulletClone = Instantiate(bullet,  new Vector3(transform.position.x,transform.position.y,transform.position.z), Quaternion.identity);
+        	BulletClone.gameObject.tag = "Bullet";
+        	clone = GetComponent<Rigidbody>();
+        	clone.AddForce(transform.forward * 500);
+        }      
+    }
+    void OnCollisionEnter(Collision col)
+        {
+        	if(col.gameObject.tag=="Enemy"){
+        		hp --;
+        		text.text = "Health:" + hp;
+        		if(hp <= 0){
+	                SceneManager.LoadScene(1);
+	                hp = 0;
+        		}
+        	}
+        }
+}
